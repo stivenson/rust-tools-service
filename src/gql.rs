5674ed012f443768e594ec984220fd46fd721c1c
Stivenson Rincón <stivenson.rpm@gmail.com>
2019-07-11 00:36:02 -0500

    refactor: add extra fields to saveTool Mutation and model

--- src/gql.rs
+++ src/gql.rs
@@ -1,5 +1,8 @@
+extern crate chrono;
+
 use bson::oid::ObjectId;
 use juniper::FieldResult;
+use chrono::{DateTime, Utc};
 
 use super::Context;
 use crate::model::Tool;
@@ -9,48 +12,74 @@ pub struct Mutations;
 
 graphql_object!(Tool: Context |&self| {
     field id() -> String { if let Some(ref id) = self.id { id.to_hex() } else { "".into() } }
-    field name() -> &str { self.name.as_str() }
-    field link() -> &str { self.link.as_str() }
-    field description() -> &str { self.description.as_str() }
+    field tool_name() -> &str { self.tool_name.as_str() }
+    field tool_link() -> &str { self.tool_link.as_str() }
     field tags() -> &str { self.tags.as_str() }
-    field additional_data() -> &str { self.additional_data.as_str() }
+    field tool_description() -> &str { self.tool_description.as_str() } // optional
+    field author_full_name() -> &str {self.author_full_name.as_str()} // optional
+    field author_email() -> &str {self.author_email.as_str()} // optional
+    field author_link() -> &str {self.author_link.as_str()} // optional
+    field clicks() -> &str { self.clicks.as_str() } // optional
+    field likes() -> &str { self.likes.as_str() } // optional
+    field additional_data() -> &str { self.additional_data.as_str() } // optional
+    field meta_data() -> &str { self.meta_data.as_str() } // optional
+    field created() -> &str { self.created.as_str() } // optional
+    field state() -> bool { self.state } // optional
 });
 
+
+// @TODO for here check to create pagination
 graphql_object!(Query: Context |&self| {
-  field apiVersion() -> &str {
-    "1.0"
-  }
+    field apiVersion() -> &str {
+      "1.0"
+    }
 
     field tools(&executor) -> FieldResult<Vec<Tool>> {
     let context = executor.context();
         Ok(context.db.list_tools()?)
     }
 
-  field tool(&executor, id: String) -> FieldResult<Option<Tool>> {
-    let context = executor.context();
-    Ok(context.db.get_tool(&id)?)
-  }
+    field tool(&executor, id: String) -> FieldResult<Option<Tool>> {
+      let context = executor.context();
+      Ok(context.db.get_tool(&id)?)
+    }
 });
 
 graphql_object!(Mutations: Context |&self| {
     field saveTool(&executor,
         id: Option<String>,
-        name: String,
-        link: String,
-        description: Option<String>,
+        tool_name: String,
+        tool_link: String,
         tags: String,
-        additional_data: Option<String>
+        tool_description: Option<String>,
+        author_full_name: Option<String>,
+        author_email: Option<String>,
+        author_link: Option<String>,
+        clicks: Option<String>,
+        likes: Option<String>,
+        additional_data: Option<String>,
+        meta_data: Option<String>, 
+        created: Option<String>, 
+        state: bool
     ) -> FieldResult<Option<Tool>> {
         let context = executor.context();
         let id = id.map(|id| ObjectId::with_string(&id)).map_or(Ok(None), |v| v.map(Some))?;
-
+        let now: DateTime<Utc> = Utc::now();
         let tool = Tool {
             id: id,
-            name: name,
-            link: link,
-            description: description.unwrap_or_else( || "".into()),
+            tool_name: tool_name,
+            tool_link: tool_link,
             tags: tags,
+            tool_description: tool_description.unwrap_or_else( || "".into()),
+            author_full_name: author_full_name.unwrap_or_else( || "".into()),
+            author_email: author_email.unwrap_or_else( || "".into()),
+            author_link: author_link.unwrap_or_else( || "".into()),
+            clicks: clicks.unwrap_or_else( || "0".into() ),
+            likes: likes.unwrap_or_else( || "0".into() ),
             additional_data: additional_data.unwrap_or_else( || "".into()),
+            meta_data: meta_data.unwrap_or_else( || "".into()),
+            created: created.unwrap_or_else( || format!("{:?}",now.format("%a %b %e %T %Y")) ),
+            state: false
         };
 
         Ok(context.db.save_tool(tool)?)
--- src/model.rs
+++ src/model.rs
@@ -6,17 +6,41 @@ pub struct Tool {
     pub id: Option<ObjectId>,
 
     #[serde(default)]
-    pub name: String,
+    pub tool_name: String,
 
     #[serde(default)]
-    pub link: String,
+    pub tool_link: String,
 
-    #[serde(default)]
-    pub description: String,
-    
     #[serde(default)]
     pub tags: String,
+
+    #[serde(default)]
+    pub tool_description: String,
+    
+    #[serde(default)]
+    pub author_full_name: String,
+
+    #[serde(default)]
+    pub author_email: String,
+
+    #[serde(default)]
+    pub author_link: String,
+
+    #[serde(default)]
+    pub clicks: String, // To call in background
+
+    #[serde(default)]
+    pub likes: String,
     
     #[serde(default)]
     pub additional_data: String,
+
+   #[serde(default)]
+    pub meta_data: String, // To call in background
+
+   #[serde(default)]
+    pub created: String,
+
+   #[serde(default)]
+    pub state: bool,
 }
